e78d02c21c5e435cb3f513fad81c4dfd58f489a0
Gaurav Bhatt <gbhatt570@gmail.com>
2026-03-27 12:57:40 +0530

    refactor: change id type from Long to Integer for customer and delivery address

--- src/main/java/com/foodservice/config/CustomMapper.java
+++ src/main/java/com/foodservice/config/CustomMapper.java
@@ -5,21 +5,19 @@ import com.foodservice.entity.dto.CustomerDTO;
 
 public class CustomMapper {
 
-        public static CustomerDTO customerToCustomerDTO(Customer customer) {
-            CustomerDTO dto = new CustomerDTO();
-            dto.setId(customer.getId());
-            dto.setName(customer.getName());
-            dto.setEmail(customer.getEmail());
-            dto.setPhone(customer.getPhone());
-            return dto;
-        }
+    public static CustomerDTO customerToCustomerDTO(Customer customer) {
+        CustomerDTO dto = new CustomerDTO();
+        dto.setCustomerName(customer.getCustomerName());
+        dto.setCustomerEmail(customer.getCustomerEmail());
+        dto.setCustomerPhone(customer.getCustomerPhone());
+        return dto;
+    }
 
-        public static Customer customerDTOToCustomer(CustomerDTO dto) {
-            Customer customer = new Customer();
-            customer.setId(dto.getId());
-            customer.setName(dto.getName());
-            customer.setEmail(dto.getEmail());
-            customer.setPhone(dto.getPhone());
-            return customer;
-        }
+    public static Customer customerDTOToCustomer(CustomerDTO dto) {
+        Customer customer = new Customer();
+        customer.setCustomerName(dto.getCustomerName());
+        customer.setCustomerEmail(dto.getCustomerEmail());
+        customer.setCustomerPhone(dto.getCustomerPhone());
+        return customer;
+    }
 }
