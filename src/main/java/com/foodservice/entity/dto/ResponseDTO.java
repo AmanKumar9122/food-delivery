e7ebe2fd8f63f9c5a1e7af57ed0b31413ce83ec6
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-03-31 17:40:30 +0530

    Resolved conflict with dev (ResponseDTO aligned with Lombok)

--- src/main/java/com/foodservice/entity/dto/ResponseDTO.java
+++ src/main/java/com/foodservice/entity/dto/ResponseDTO.java
@@ -9,38 +9,4 @@ public class ResponseDTO {
     private Integer statusCode;
     private String message;
     private Object data;
-    public ResponseDTO() {}
-    
-        public ResponseDTO(int statusCode, String message, Object data) {
-            this.statusCode = statusCode;
-            this.message = message;
-            this.data = data;
-        }
-
-        public int getStatusCode() {
-            return statusCode;
-        }
-
-        public void setStatusCode(int statusCode) {
-            this.statusCode = statusCode;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public Object getData() {
-            return data;
-        }
-
-        public void setData(Object data) {
-            this.data = data;
-        }
-    
-    
-    
 }
